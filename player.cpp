d501ee7062948531a1d5521b72c9c70abcb176e6
Lucas Linhaus <53151377+LucasLinhaus@users.noreply.github.com>
2019-08-14 20:02:04 -0300

    Ideia da Ficha do personagem
    Primeira tentativa de adicionar a ficha ao personagem jogável

---
+++ player.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int Player(char a, b, c){          //Oq fazer, atributo, valor//
+     char Name[10];
+     float Level=1, Xp=0, Hp=15, Atk=1, Def=1, Spd=1 ,Gold_Coins=0;
+     
+     //Perguntar//
+     
+     if(a=='p'){
+          
+     }
+     
+     //Editar//
+     
+     else if(a=='e'){
+          
+     }
+}
