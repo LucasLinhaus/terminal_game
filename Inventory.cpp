6f132a6293ce368695978a77a4f6ccdd0dc9c679
Isaac G. Y. Rabello <50160939+isaacyounes04@users.noreply.github.com>
2019-10-07 11:46:06 -0300

    Add files via upload
    Princípio do código para gerenciar atributos de força e 'crs';
    
    Atualizações : Acrescentar funções para leitura e escrita dos atributos no .txt

---
+++ Inventory.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int leitura(){
+	
+}
+
+int player(){
+	char Name[10];
+	int equip, forca=0, crs=0, hp=30;    // add leitura do arquivo de texto pra inserir valor no int e nome do player //
+	equip=;                         // Ler do arq de texto o id do equipamento //
+	if(equip=0){
+		forca=0;
+		crs=0;
+	}else if(equip=1){
+		forca=forca+[...];   // add valor e IFs de acordo com itens //
+		crs=crs+[...];
+	}
+	
+	// reescrever valores no arq de texto //
+	
+}
